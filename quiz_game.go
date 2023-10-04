6156645bd3789845e1d8913fea49c94cd3dcc474
ikotun-dev <danlogan2003@gmail.com>
2023-10-04 20:14:45 +0100

    Additions

---
+++ quiz_game.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() { 
+	fmt.Println("Welcome to my Quiz game-- ")
+	//types : int = -9, uint = 90, string = "mean", float64 = 9.5, bool = "true/false"
+	//have to use precision here in golang 
+	name := "Joe"
+	fmt.Printf("Hello %v, how are you doing?", name)
+	
+
+}
