941255b377ec5ae7151e060f5a38df7a36a39362
ikotun-dev <danlogan2003@gmail.com>
2023-10-04 20:28:56 +0100

    crud

---
+++ crud/main.go
@@ -0,0 +1,22 @@
+package main
+import (
+	"fmt",
+	"log",
+	"encoding/json",
+	"math/rand",
+	"net/http",
+	"strconv",
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+	
+}
+
+type Director struct { 
+
+}
