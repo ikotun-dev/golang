94a2750a3c79482f926faf612758da165f9f935b
ikotun-dev <danlogan2003@gmail.com>
2023-10-05 12:55:17 +0100

    Added cool stuff

--- crud/main.go
+++ crud/main.go
@@ -23,12 +23,71 @@ type Director struct {
 }
 
 var movies []Movie
+
+//function to get all movies 
 func getMovies(w http.ResponseWriter, r *http.Request){
-	w.Header.
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
 }
 
+//function to delete a movie 
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["ID"]{
+			movies = append(movies[:index], movies[index+1]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+	
+}
+//function to get a single movie 
+func getMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies { 
+		if item.ID == params["ID"]{
+			json.NewEncoder(w).Encode(item)
+
+			return
+		}
+	}
+}
+
+
+//main function 
 func main() { 
 	r := mux.NewRouter()
 
-	r.Handle
+	movie1 := Movie{
+		ID : "1",
+		Isbn : "12345",
+		Title : "Spiderman",
+		Director:  : &Director{
+			Firstname : "John",
+			Lastname : "Doe"
+		},
+	}
+
+	movie2 := Movie{
+		ID : "2",
+		Isbn : "12323",
+		Title : "Superman",
+		Director:  : &Director{
+			Firstname : "John",
+			Lastname : "Doe"
+		},
+	}
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", updateMovie).Methods("PUT")	
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
 }
+
+
